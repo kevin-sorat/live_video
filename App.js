c53f80cd603f3344509cbfd8d97e8fc4c5296fa9
Kevin Sorat <kevinsorat@Kevins-MBP-2.fios-router.home>
2020-11-29 18:47:28 -0500

    First commit

---
+++ App.js
@@ -0,0 +1,146 @@
+// React Native Video Library to Play Video in Android and IOS
+// https://aboutreact.com/react-native-video/
+
+// import React in our code
+import React, {useState, useRef} from 'react';
+
+// import all the components we are going to use
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+//Import React Native Video to play video
+import Video from 'react-native-video';
+
+//Media Controls to control Play/Pause/Seek and full screen
+import
+  MediaControls, {PLAYER_STATES}
+from 'react-native-media-controls';
+
+const App = () => {
+  const videoPlayer = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [paused, setPaused] = useState(false);
+  const [
+    playerState, setPlayerState
+  ] = useState(PLAYER_STATES.PLAYING);
+  const [screenType, setScreenType] = useState('content');
+
+  const onSeek = (seek) => {
+    //Handler for change in seekbar
+    videoPlayer.current.seek(seek);
+  };
+
+  const onPaused = (playerState) => {
+    //Handler for Video Pause
+    setPaused(!paused);
+    setPlayerState(playerState);
+  };
+
+  const onReplay = () => {
+    //Handler for Replay
+    setPlayerState(PLAYER_STATES.PLAYING);
+    videoPlayer.current.seek(0);
+  };
+
+  const onProgress = (data) => {
+    // Video Player will progress continue even if it ends
+    if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
+      setCurrentTime(data.currentTime);
+    }
+  };
+
+  const onLoad = (data) => {
+    setDuration(data.duration);
+    setIsLoading(false);
+  };
+
+  const onLoadStart = (data) => setIsLoading(true);
+
+  const onEnd = () => setPlayerState(PLAYER_STATES.ENDED);
+
+  const onError = () => alert('Oh! ', error);
+
+  const exitFullScreen = () => {
+    alert('Exit full screen');
+  };
+
+  const enterFullScreen = () => {};
+
+  const onFullScreen = () => {
+    setIsFullScreen(isFullScreen);
+    if (screenType == 'content') setScreenType('cover');
+    else setScreenType('content');
+  };
+
+  const renderToolbar = () => (
+    <View>
+      <Text style={styles.toolbar}> toolbar </Text>
+    </View>
+  );
+
+  const onSeeking = (currentTime) => setCurrentTime(currentTime);
+
+  return (
+    <View style={{flex: 1}}>
+      <Video
+        onEnd={onEnd}
+        onLoad={onLoad}
+        onLoadStart={onLoadStart}
+        onProgress={onProgress}
+        paused={paused}
+        ref={videoPlayer}
+        resizeMode={screenType}
+        onFullScreen={isFullScreen}
+        source={{
+          // Run this curl command to generate Playback ID and Stream key:
+          // curl https://api.mux.com/video/v1/live-streams -H "Content-Type: application/json" -X POST -d '{ "playback_policy": ["public"], "new_asset_settings": { "playback_policy": ["public"] } }' -u <MUX_TOKEN_ID>:<MUX_TOKEN_SECRET>
+          // Example:
+          // curl https://api.mux.com/video/v1/live-streams -H "Content-Type: application/json" -X POST -d '{ "playback_policy": ["public"], "new_asset_settings": { "playback_policy": ["public"] } }' -u 69c3510d-88cf-4c6a-82a6-c83f43309aef:57VmhADkgKA+1BjF9hmeALFuFOUCCAoTF7quPTDtjvz64cPSeEwyo9pmbekGabZsyZypnNR3aqj
+          
+          // uri: "https://stream.mux.com/PLAYBACK_ID_HERE.m3u8"
+          uri: "https://stream.mux.com/01C01LeSsd017tO01f00ShMJQPId98lkbI02nwW9fQaUIukC4.m3u8"
+        }}
+        style={styles.mediaPlayer}
+        volume={10}
+      />
+      <MediaControls
+        duration={duration}
+        isLoading={isLoading}
+        mainColor="#333"
+        onFullScreen={onFullScreen}
+        onPaused={onPaused}
+        onReplay={onReplay}
+        onSeek={onSeek}
+        onSeeking={onSeeking}
+        playerState={playerState}
+        progress={currentTime}
+        toolbar={renderToolbar()}
+      />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  toolbar: {
+    marginTop: 30,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  mediaPlayer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+});
